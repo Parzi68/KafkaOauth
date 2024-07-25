12c22b44144d8d5ec3d6c3a71c5e242c6024ae1c
Your Name <you@example.com>
2024-07-25 18:33:26 +0530

    Added AdminAccessView class.

---
+++ src/main/java/com/example/kafkaoauth/AdminAccessView.java
@@ -0,0 +1,10 @@
+// src/main/java/com/example/kafkaoauth/AdminAccessView.java
+package com.example.kafkaoauth;
+
+import com.vaadin.flow.component.page.ViewportMeta;
+import com.vaadin.flow.router.View;
+
+@View(name = "admin-access")
+@ViewportMeta(name = "viewport", content = "width=device-width, initial-scale=1.0")
+public class AdminAccessView extends AdminAccessUI {
+}
